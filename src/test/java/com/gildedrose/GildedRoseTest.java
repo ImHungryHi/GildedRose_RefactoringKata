01a464fcd93d2661a39ad143c73b5be6a9702020
ChrisDS <chris.de.smedt.91@gmail.com>
2021-10-17 16:40:31 +0200

    First test - backstage passes expire

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,22 @@
 package com.gildedrose;
-
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
-
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void should_Reset_WhenBackstagePasses_Expire() {
+        // Given
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49) };
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
 
+        // When
+        app.updateQuality();
+
+        // Then
+        assertAll(
+            () -> assertEquals(-1, app.items[0].sellIn),
+            () -> assertEquals(0, app.items[0].quality)
+        );
+    }
 }
