8ff9b845db12622e3c7a6439d53c95aa2241689d
ChrisDS <chris.de.smedt.91@gmail.com>
2021-10-17 17:56:06 +0200

    Refactored code to my liking - functional & tests passed

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 class GildedRose {
     public static final int MAX_QUALITY = 50;
+    public static final int SULFURAS_QUALITY = 80;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -19,61 +20,39 @@ class GildedRose {
     //     - [Sulfuras] -> no SellIn applies, quality remains as is (=80)
     //     - [Backstage passes] -> quality increases instead of decreasing; increasing by 2
     //          if SellIn < 11 and by 3 if SellIn < 6; drops to 0 when SellIn is negative
-    //-------- CURRENT IN CODE --------
-    //  - [Aged Brie] -> increment quality if it drops below 50, once if sellIns are >= 0, twice otherwise
-    //  - [Backstage passes] -> reset quality to 0 if sellIn is negative, else increment twice if quality
-    //       is under 50 and sellIn is less than 11, once more when sellIn is 5 or less
-    //  - [Sulfuras] -> don't decrement sellIn
-    //  - Everything else -> decrement quality by 1 if >= 0, once more if sellIn is negative
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+            if (!item.name.contains("Sulfuras")) {
+                item.sellIn--;
+                int qualityChangeMultiplier = (item.name.startsWith("Conjured") || item.sellIn < 0) ? 2 : 1;
+
+                if (item.name.contains("Aged Brie") ||
+                    (item.name.contains("Backstage passes") && item.sellIn > 10)) {
+                    item.quality += qualityChangeMultiplier;
+                }
+                else if (item.name.contains("Backstage passes")) {
+                    if (item.sellIn < 0) {
+                        item.quality = 0;
+                    }
+                    else if (item.sellIn < 6) {
+                        item.quality += 3 * qualityChangeMultiplier;
+                    } else if (item.sellIn < 11) {
+                        item.quality += 2 * qualityChangeMultiplier;
                     }
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                else if (item.quality > 0) {
+                    item.quality -= qualityChangeMultiplier;
+                }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+                if (item.quality > 50) {
+                    item.quality = 50;
+                }
+                else if (item.quality < 0) {
+                    item.quality = 0;
                 }
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            else if (item.quality != SULFURAS_QUALITY) {
+                item.quality = SULFURAS_QUALITY;
             }
         }
     }
