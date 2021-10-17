fffce8771848f6c790035cb91539490c83a1e418
ChrisDS <chris.de.smedt.91@gmail.com>
2021-10-17 16:47:04 +0200

    Interpreted rules and added max quality constant

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,12 +1,30 @@
 package com.gildedrose;
 
 class GildedRose {
+    public static final int MAX_QUALITY = 50;
     Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    // Rules to play by:
+    //  - SellIn -> days left to sell product
+    //  - Quality -> How valuable the item is
+    //  - At the end of the day, quality is updated for all items
+    //     - 0 >= Quality > 50
+    //     - SellIn negative -> quality degrades twice as fast
+    //     - [Conjured] -> quality degrades twice as fast (OR stmt in tandem with above)
+    //     - [Aged Brie] -> quality increases instead of decreasing
+    //     - [Sulfuras] -> no SellIn applies, quality remains as is (=80)
+    //     - [Backstage passes] -> quality increases instead of decreasing; increasing by 2
+    //          if SellIn < 11 and by 3 if SellIn < 6; drops to 0 when SellIn is negative
+    //-------- CURRENT IN CODE --------
+    //  - [Aged Brie] -> increment quality if it drops below 50, once if sellIns are >= 0, twice otherwise
+    //  - [Backstage passes] -> reset quality to 0 if sellIn is negative, else increment twice if quality
+    //       is under 50 and sellIn is less than 11, once more when sellIn is 5 or less
+    //  - [Sulfuras] -> don't decrement sellIn, increment quality by 1 if under 50
+    //  - Everything else -> decrement quality by 1 if >= 0, once more if sellIn is negative
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
