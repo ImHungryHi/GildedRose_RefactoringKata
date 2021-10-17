d5060214750fb9cd003764485428a3b2aec7369b
ChrisDS <chris.de.smedt.91@gmail.com>
2021-10-17 18:26:04 +0200

    Further refactoring for descriptiveness

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,6 +3,18 @@ package com.gildedrose;
 class GildedRose {
     public static final int MAX_QUALITY = 50;
     public static final int SULFURAS_QUALITY = 80;
+
+    private static final int MINIMAL_QUALITY = 0;
+    private static final int MINIMAL_SELLIN_DAYS = 0;
+    private static final int NORMAL_DEGRADING = 1;
+    private static final int RAPID_DEGRADING = 2;
+    private static final int RARE_TICKET_DAYS_LEFT = 11;
+    private static final int LEGENDARY_TICKET_DAYS_LEFT = 6;
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes";
+    private static final String CONJURED = "Conjured";
+    private static final String SULFURAS = "Sulfuras";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -22,18 +34,18 @@ class GildedRose {
     //          if SellIn < 11 and by 3 if SellIn < 6; drops to 0 when SellIn is negative
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.contains("Sulfuras")) {
+            if (!item.name.contains(SULFURAS)) {
                 item.sellIn--;
                 int qualityChangeMultiplier = checkSellInNegativeOrConjured(item);
 
-                if (item.name.contains("Aged Brie")) {
+                if (item.name.contains(AGED_BRIE)) {
                     item.quality += qualityChangeMultiplier;
                 }
-                else if (item.name.contains("Backstage passes")) {
+                else if (item.name.contains(BACKSTAGE_PASSES)) {
                     calculateBackstagePassQuality(item, qualityChangeMultiplier);
                 }
                 // Default, generic item
-                else if (item.quality > 0) {
+                else if (item.quality > MINIMAL_QUALITY) {
                     item.quality -= qualityChangeMultiplier;
                 }
 
@@ -47,29 +59,29 @@ class GildedRose {
     }
 
     private int checkSellInNegativeOrConjured(Item item) {
-        if (item.name.startsWith("Conjured") || item.sellIn < 0) {
-            return 2;
+        if (item.name.startsWith(CONJURED) || item.sellIn < MINIMAL_SELLIN_DAYS) {
+            return RAPID_DEGRADING;
         }
 
-        return 1;
+        return NORMAL_DEGRADING;
     }
 
     private void checkQualityConstraints(Item item) {
         if (item.quality > MAX_QUALITY) {
             item.quality = MAX_QUALITY;
         }
-        else if (item.quality < 0) {
-            item.quality = 0;
+        else if (item.quality < MINIMAL_QUALITY) {
+            item.quality = MINIMAL_QUALITY;
         }
     }
 
     private void calculateBackstagePassQuality(Item item, int qualityChangeMultiplier) {
-        if (item.sellIn < 0) {
-            item.quality = 0;
+        if (item.sellIn < MINIMAL_SELLIN_DAYS) {
+            item.quality = MINIMAL_QUALITY;
         }
-        else if (item.sellIn < 6) {
+        else if (item.sellIn < LEGENDARY_TICKET_DAYS_LEFT) {
             item.quality += 3 * qualityChangeMultiplier;
-        } else if (item.sellIn < 11) {
+        } else if (item.sellIn < RARE_TICKET_DAYS_LEFT) {
             item.quality += 2 * qualityChangeMultiplier;
         }
         else {
