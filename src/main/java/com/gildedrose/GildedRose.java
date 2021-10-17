1a38b0cd7664d4986028cbc66e01dc7c033754e8
ChrisDS <chris.de.smedt.91@gmail.com>
2021-10-17 18:06:31 +0200

    Refactored - extracted longer code into descriptively named functions

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -24,36 +24,56 @@ class GildedRose {
         for (Item item : items) {
             if (!item.name.contains("Sulfuras")) {
                 item.sellIn--;
-                int qualityChangeMultiplier = (item.name.startsWith("Conjured") || item.sellIn < 0) ? 2 : 1;
+                int qualityChangeMultiplier = checkSellInNegativeOrConjured(item);
 
-                if (item.name.contains("Aged Brie") ||
-                    (item.name.contains("Backstage passes") && item.sellIn > 10)) {
+                if (item.name.contains("Aged Brie")) {
                     item.quality += qualityChangeMultiplier;
                 }
                 else if (item.name.contains("Backstage passes")) {
-                    if (item.sellIn < 0) {
-                        item.quality = 0;
-                    }
-                    else if (item.sellIn < 6) {
-                        item.quality += 3 * qualityChangeMultiplier;
-                    } else if (item.sellIn < 11) {
-                        item.quality += 2 * qualityChangeMultiplier;
-                    }
+                    calculateBackstagePassQuality(item, qualityChangeMultiplier);
                 }
+                // Default, generic item
                 else if (item.quality > 0) {
                     item.quality -= qualityChangeMultiplier;
                 }
 
-                if (item.quality > 50) {
-                    item.quality = 50;
-                }
-                else if (item.quality < 0) {
-                    item.quality = 0;
-                }
+                checkQualityConstraints(item);
             }
+            // Sulfuras doesn't change, but keep tabs so that the quality remains 80
             else if (item.quality != SULFURAS_QUALITY) {
                 item.quality = SULFURAS_QUALITY;
             }
+        }
+    }
+
+    private int checkSellInNegativeOrConjured(Item item) {
+        if (item.name.startsWith("Conjured") || item.sellIn < 0) {
+            return 2;
+        }
+
+        return 1;
+    }
+
+    private void checkQualityConstraints(Item item) {
+        if (item.quality > MAX_QUALITY) {
+            item.quality = MAX_QUALITY;
+        }
+        else if (item.quality < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void calculateBackstagePassQuality(Item item, int qualityChangeMultiplier) {
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+        else if (item.sellIn < 6) {
+            item.quality += 3 * qualityChangeMultiplier;
+        } else if (item.sellIn < 11) {
+            item.quality += 2 * qualityChangeMultiplier;
+        }
+        else {
+            item.quality += qualityChangeMultiplier;
         }
     }
 }
